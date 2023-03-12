d69a87c0922dcc6e41c809e9956c07139eae218d
KimberlyW618 <kimw618@gmail.com>
2023-03-11 20:53:44 -0800

    commit3

--- Analysis_Projects_Folder/Pewlett-Hackard_Analysis_Folder/Queries/schema.sql
+++ Analysis_Projects_Folder/Pewlett-Hackard_Analysis_Folder/Queries/schema.sql
@@ -1,23 +1,23 @@
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
-     dept_no VARCHAR(4) NOT NULL,
-     dept_name VARCHAR(40) NOT NULL,
-     PRIMARY KEY (dept_no),
-     UNIQUE (dept_name)
+	dept_no VARCHAR(4) NOT NULL,
+    dept_name VARCHAR(40) NOT NULL,
+    PRIMARY KEY (dept_no),
+    UNIQUE (dept_name)
 );
 
 CREATE TABLE employees (
-	 emp_no INT NOT NULL,
-     birth_date DATE NOT NULL,
-     first_name VARCHAR NOT NULL,
-     last_name VARCHAR NOT NULL,
-     gender VARCHAR NOT NULL,
-     hire_date DATE NOT NULL,
-     PRIMARY KEY (emp_no)
+	emp_no INT NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    gender VARCHAR NOT NULL,
+    hire_date DATE NOT NULL,
+    PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -27,18 +27,19 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 );
 
 CREATE TABLE salaries (
-  emp_no INT NOT NULL,
-  salary INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+	emp_no INT NOT NULL,
+ 	salary INT NOT NULL,
+ 	from_date DATE NOT NULL,
+ 	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
 );
+
+DROP TABLE dept_emp
 
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR NOT NULL,
-	birth_date DATE NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -46,13 +47,24 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 
+DROP TABLE titles CASCADE
 CREATE TABLE titles (
  	emp_no INT NOT NULL,
-	titles VARCHAR NOT NULL,
+	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
+-- Make sure all the data has imported for each table
 SELECT * FROM departments;
+
+SELECT * FROM employees;
+
+SELECT * FROM dept_manager;
+
+SELECT * FROM salaries;
+
+SELECT * FROM dept_emp;
+
+SELECT * FROM titles;
